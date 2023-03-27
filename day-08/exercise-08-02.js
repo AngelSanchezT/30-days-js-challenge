1525a056bd643ea66c66093f24b0f8b4f6b34434
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-03-26 21:32:38 -0500

    Day 08-02 - Map Create your own method

--- day-08/exercise-08-02.js
+++ day-08/exercise-08-02.js
@@ -1,10 +1,16 @@
 export function myMap(array, func) {
-  
+  // Creamos el array que retornaremos
+  // Inicialmente está vacío
+  // Es importante recalcar que en ningún momento modificamos el array original
   const newArray = [];
 
   for (let i = 0; i < array.length; i++) {
+    // Luego iteramos por cada elemento del array
+    // Y lo vamos pasando al array vacío después de ejecutar 
+    // la función sobre cada elemento del array
     newArray.push(func(array[i]));
   }
 
+  // Al final lo retornamos
   return newArray;
 }
