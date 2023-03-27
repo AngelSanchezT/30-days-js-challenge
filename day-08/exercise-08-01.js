adb11b74fff1a5059948cdc0c149a41672d5c3be
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-03-26 20:25:12 -0500

    Day 08 - Create a calculator with closures

--- day-08/exercise-08-01.js
+++ day-08/exercise-08-01.js
@@ -1,30 +1,44 @@
-export function createCalculator() {
-  // Tu código aquí 👈
-  let privateValue = 0;
-
-  return {
-    add(val) {
-        privateValue += val;
-        return privateValue;
-    },
-    subtract(val) {
-        privateValue -= val;
-        return privateValue;
-    },
-    multiply(val) {
-        privateValue *= val;
-        return privateValue;
-    },
-    divide(val) {
-        privateValue /= val;
-        return privateValue;
-    },
-    clear(val) {
-        privateValue = 0;
-        return privateValue;
-    },
-    getTotal() {
-        return privateValue;
-    }
+function createCalculator() {
+    // Primero declaramos nuestra variable privada
+    // que tendrá un valor inicial de 0
+    let total = 0;
+  
+    // dentro del return devolvemos las funciones que modifican la variable privada
+    return {
+      add(value) {
+        // Para sumar, simplemente sumamos el valor al total
+        // y retornamos el total
+        total += value;
+        return total;
+      },
+  
+      subtract(value) {
+       // Lo mismo con la resta 
+        total -= value;
+        return total;
+      },
+  
+      multiply(value) {
+        // Lo mismo con la multiplicación y división
+        total *= value;
+        return total;
+      },
+  
+      divide(value) {
+        total /= value;
+        return total;
+      },
+  
+      // Al final una función para reiniciar el valor a 0
+      clear() {
+        total = 0;
+        return total;
+      },
+  
+      getTotal() {
+        // Y el total por si se quiere guardar el valor en otra variable en cierto momento
+        return total;
+      },
+    };
   }
-}
+  
