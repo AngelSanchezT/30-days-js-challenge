b985950978ed41ad40629824d516bb356386acf4
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-03-28 22:48:42 -0500

    Day 11 - Exersice 01 - create a promise to send emails - Unit Test

--- day-11/exercise-11-01.js
+++ day-11/exercise-11-01.js
@@ -1,3 +1,10 @@
 export function sendEmail(email, subject, body) {
   // Tu código aquí 👈
+  
+  if(email === undefined || email.length === 0) {
+    throw new Error("Error email is missing");
+    
+  }
+
+  return { email, subject, body};
 }
--- day-11/test/exercise-11-01.test.js
+++ day-11/test/exercise-11-01.test.js
@@ -1,20 +1,28 @@
 import assert from "assert";
+import { expect } from "chai";
+import sinon from "sinon";
+
 import { sendEmail } from "../exercise-11-01";
 
 describe("🌐 Day 11 - Create a Promise to Send Emails", () => {
   it("should call setTimeout with 2s", async () => {
-    const spy = jest.spyOn(global, "setTimeout");
+    // const spy = jest.spyOn(global, "setTimeout");
+
+    // create a spy for the method setTimeout
+    const spy = sinon.spy(global, "setTimeout");
 
     const email = "user@example.com";
     const subject = "Test Subject";
     const body = "Test Body";
+    // Call the method
     const rta = await sendEmail(email, subject, body);
 
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenLastCalledWith(expect.any(Function), 2000);
-    expect(rta).toEqual({ email, subject, body });
-    
-
+    // We check that the method of the object has been called exactly once
+    // and that the last call was made with a function and a numeric
+    // argument equal to 2000.
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.lastCall.args).to.eql([expect.any(Function), 2000]);
+    expect(rta).to.eql({ email, subject, body });
   });
 
   it("should resolve with email information after 2 seconds", async () => {
@@ -23,7 +31,7 @@ describe("🌐 Day 11 - Create a Promise to Send Emails", () => {
     const body = "Test Body";
     const emailInfo = await sendEmail(email, subject, body);
 
-    expect(emailInfo).toEqual({ email, subject, body });
+    expect(emailInfo).to.eql({ email, subject, body });
   });
 
   it("should reject if email is missing", async () => {
@@ -31,7 +39,9 @@ describe("🌐 Day 11 - Create a Promise to Send Emails", () => {
     const subject = "Test Subject";
     const body = "Test Body";
 
-    await expect(sendEmail(email, subject, body)).rejects.toThrow();
+    await expect(() => {
+      sendEmail(email, subject, body);
+    }).to.throw(Error, "Error email is missing");
   });
 
   it("should reject if subject is missing", async () => {
@@ -39,7 +49,9 @@ describe("🌐 Day 11 - Create a Promise to Send Emails", () => {
     const subject = "";
     const body = "Test Body";
 
-    await expect(sendEmail(email, subject, body)).rejects.toThrow();
+    await expect(() => {
+        sendEmail(email, subject, body);
+      }).to.throw(Error, "Error subject is missing");
   });
 
   it("should reject if body is missing", async () => {
@@ -47,6 +59,8 @@ describe("🌐 Day 11 - Create a Promise to Send Emails", () => {
     const subject = "Test Subject";
     const body = "";
 
-    await expect(sendEmail(email, subject, body)).rejects.toThrow();
+    await expect(() => {
+        sendEmail(email, subject, body);
+      }).to.throw(Error, "Error body is missing");
   });
 });
