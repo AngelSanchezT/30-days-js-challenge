1704505201d93af676bba11016f490efc5e0b1bf
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-04-01 23:56:05 -0500

    :red_circle:  Modify a shopping list - init exercise-14-01.js

---
+++ day-14/test/exercise-14-01.test.js
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+
+import { processShoppingList } from "../exercise-14-01";
+
+const shoppingList = [
+  { name: "pan", price: 20, quantity: 2 },
+  { name: "leche", price: 25, quantity: 1 },
+  { name: "oferta manzanas", price: 10, quantity: 3 },
+];
+
+describe("Day 14-01 - Modify a shopping list", () => {
+  it("should return the total", () => {
+    const rta = processShoppingList(shoppingList);
+    expect(rta).to.be.a(89);
+  });
+
+  it("should apply the discount to items", () => {
+    const item = shoppingList.find((i) => i.name.includes("oferta"));
+    expect(item.price).to.be.a(24);
+  });
+
+  it("should delete the quantity property", () => {
+    const item = shoppingList[0];
+    expect(item.quantity).to.be.a(undefined);
+  });
+
+  it("should modify the original array", () => {
+    expect(shoppingList).to.eql([
+      { name: "pan", price: 40 },
+      { name: "leche", price: 25 },
+      { name: "oferta manzanas", price: 24 },
+    ]);
+  });
+});
