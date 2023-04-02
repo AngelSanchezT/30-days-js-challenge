cb3fbc8cfb9b17b9022a650d89e1da7e38cea599
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-04-01 23:29:13 -0500

    :white_check_mark: Day 13-02 - Find the location of the searched value

--- day-13/exercise-13-02.js
+++ day-13/exercise-13-02.js
@@ -1,3 +1,17 @@
 export function searchValue(array, value) {
   // Tu código aquí 👈
+  let result;
+  array.forEach((element, index) => {
+    const valueIndex = element.indexOf(value);
+
+    if (valueIndex > -1) {
+      result = { row: index, column: valueIndex };
+    }
+  });
+
+  if (result === undefined) {
+    throw new Error("Valor no encontrado");
+  }
+
+  return result;
 }
