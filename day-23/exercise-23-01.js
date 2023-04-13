682d20d1b33c82580304f25ecf8aebc5686ce3d7
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-04-12 23:47:46 -0500

    :white_check_mark: Day 23-01 - Proxy in messaging service

--- day-23/exercise-23-01.js
+++ day-23/exercise-23-01.js
@@ -1,13 +1,22 @@
 export class MessagesProxy {
   constructor(messages, user) {
-    // Tu código aquí 👈
+    this.messages = messages;
+    this.user = user;
   }
 
   sendMessage(text) {
-    // Tu código aquí 👈
+    if (this.user.isLoggedIn()) {
+      this.messages.sendMessage(text);
+    } else {
+      throw new Error("Usuario no logueado");
+    }
   }
 
   getHistory() {
-    // Tu código aquí 👈
+    if (this.user.isLoggedIn()) {
+      return this.messages.getHistory();
+    } else {
+      throw new Error("Usuario no logueado");
+    }
   }
 }
