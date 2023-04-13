e5be4081faa0883e241e5f451624a39c1b2df113
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-04-12 22:41:07 -0500

    :white_check_mark: Day 21 - Implement singleton in a chat

--- day-21/exercise-21.js
+++ day-21/exercise-21.js
@@ -1,5 +1,30 @@
 import { User } from "./user";
-
 export class Chat {
-  // Tu código aquí 👈
+  constructor() {
+    if (!Chat.instance) {
+      this.createInstance();
+      Chat.instance = this;
+    }
+    return Chat.instance;
+  }
+
+  createInstance() {
+    this.users = [];
+  }
+
+  sendMessage(message) {
+    this.users.forEach((user) => {
+      user.receiveMessage(message);
+    });
+  }
+
+  addUser(user) {
+    if (user instanceof User) {
+      this.users.push(user);
+    }
+  }
+
+  removeUser(user) {
+    this.users = this.users.filter((u) => u.name !== user);
+  }
 }
