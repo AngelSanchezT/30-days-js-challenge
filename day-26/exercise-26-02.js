2604b5c40049a36dae7f5514eec5caa9f99cef5c
Angel Javier Sanchez <ajsanchez22@gmail.com>
2023-04-15 01:08:15 -0500

    :white_check_mark: Day-26-02 - Create a task organizer

--- day-26/exercise-26-02.js
+++ day-26/exercise-26-02.js
@@ -1,3 +1,19 @@
 export function taskManager() {
-  // Tu código aquí 👈
+  const tasks = new Map();
+
+  const addTask = function (task, tags) {
+    task = task.toLowerCase();
+    if (tasks.has(task)) {
+      const existingTags = tasks.get(task);
+      tags.forEach((tag) => existingTags.add(tag));
+    } else {
+      const newTags = new Set(tags);
+      tasks.set(task, newTags);
+    }
+  };
+
+  const printTasks = function () {
+    return tasks;
+  };
+  return { addTask, printTasks };
 }
